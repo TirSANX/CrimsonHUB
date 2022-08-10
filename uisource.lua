8b31173fd8a3532296ac774299f07f204dee02a8
TirSANX <109739122+TirSANX@users.noreply.github.com>
2022-08-10 23:05:53 +0700

    Update uisource.lua

--- uisource.lua
+++ uisource.lua
@@ -19,7 +19,7 @@ local themes = {
     Accent = Color3.fromRGB(10, 10, 10),
     LightContrast = Color3.fromRGB(20, 20, 20),
     DarkContrast = Color3.fromRGB(14, 14, 14),  
-    TextColor = Color3.fromRGB(255, 255, 255)
+    TextColor = Color3.fromRGB(255, 61, 61)
 }
 
 do
