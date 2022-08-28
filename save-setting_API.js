8709f63f65256eb2c44dab924da80bc67e6f8e73
TirSANX <109739122+TirSANX@users.noreply.github.com>
2022-08-29 00:00:18 +0700

    Update save-setting_API.js

--- save-setting_API.js
+++ save-setting_API.js
@@ -1,4 +1,4 @@
-getgenv()['Crimson_Config'] = game.Players.LocalPlayer.Name
+getgenv()['MyName'] = game.Players.LocalPlayer.Name
 getgenv()['JsonEncode'] = function(msg)
     return game:GetService("HttpService"):JSONEncode(msg)
 end
@@ -6,24 +6,24 @@ getgenv()['JsonDecode'] = function(msg)
     return game:GetService("HttpService"):JSONDecode(msg)
 end
 getgenv()['Check_Setting'] = function(Name)
-    if not isfolder('SaveSetting') then
-        makefolder('SaveSetting')
+    if not isfolder('Crimson_Config') then
+        makefolder('Crimson_Config')
     end
-    if not isfile('SaveSetting/'..Name..'_Config'..'.json') then
-        writefile('SaveSetting/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
+    if not isfile('Crimson_Config/'..Name..'_Config'..'.json') then
+        writefile('Crimson_Config/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
     end
 end
 getgenv()['Get_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_Config'..'.json') then
-        getgenv().Setting = JsonDecode(readfile('SaveSetting/'..Name..'_Config'..'.json'))
+    if isfolder('Crimson_Config') and isfile('Crimson_Config/'..Name..'_Config'..'.json') then
+        getgenv().Setting = JsonDecode(readfile('Crimson_Config/'..Name..'_Config'..'.json'))
         return getgenv().Setting
     else
         Check_Setting(Name)
     end
 end
 getgenv()['Update_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_Config'..'.json') then
-        writefile('SaveSetting/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
+    if isfolder('Crimson_Config') and isfile('Crimson_Config/'..Name..'_Config'..'.json') then
+        writefile('Crimson_Config/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
     else
         Check_Setting(Name)
     end
