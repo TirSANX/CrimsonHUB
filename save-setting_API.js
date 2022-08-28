60533df2f3f5456087e0afea4e7e7f13b1f9ddcb
TirSANX <109739122+TirSANX@users.noreply.github.com>
2022-08-28 23:58:39 +0700

    Update save-setting_API.js

--- save-setting_API.js
+++ save-setting_API.js
@@ -9,21 +9,21 @@ getgenv()['Check_Setting'] = function(Name)
     if not isfolder('SaveSetting') then
         makefolder('SaveSetting')
     end
-    if not isfile('SaveSetting/'..Name..'_'..'.json') then
-        writefile('SaveSetting/'..Name..'_'..'.json',JsonEncode(getgenv().Setting))
+    if not isfile('SaveSetting/'..Name..'_Config'..'.json') then
+        writefile('SaveSetting/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
     end
 end
 getgenv()['Get_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'.json') then
-        getgenv().Setting = JsonDecode(readfile('SaveSetting/'..Name..'_'..'.json'))
+    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_Config'..'.json') then
+        getgenv().Setting = JsonDecode(readfile('SaveSetting/'..Name..'_Config'..'.json'))
         return getgenv().Setting
     else
         Check_Setting(Name)
     end
 end
 getgenv()['Update_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'.json') then
-        writefile('SaveSetting/'..Name..'_'..'.json',JsonEncode(getgenv().Setting))
+    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_Config'..'.json') then
+        writefile('SaveSetting/'..Name..'_Config'..'.json',JsonEncode(getgenv().Setting))
     else
         Check_Setting(Name)
     end
