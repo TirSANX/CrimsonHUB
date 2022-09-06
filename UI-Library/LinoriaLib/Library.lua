582ba81904d8cbd1a4c8875eb4de0c7098983c45
TirSANX <109739122+TirSANX@users.noreply.github.com>
2022-09-06 21:30:51 +0700

    Update Library.lua

--- UI-Library/LinoriaLib/Library.lua
+++ UI-Library/LinoriaLib/Library.lua
@@ -33,10 +33,10 @@ local Library = {
 
     HudRegistry = {};
 
-    FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
+    FontColor = Color3.fromRGB(255, 61, 61);
+    MainColor = Color3.fromRGB(25, 25, 25);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    AccentColor = Color3.fromRGB(255, 61, 61);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
