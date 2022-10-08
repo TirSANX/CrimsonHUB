d00fa7204e33c5bc049ef67103f197955395dae9
TirSANX <109739122+TirSANX@users.noreply.github.com>
2022-10-08 20:49:44 +0700

    Update BFChest.lua

--- BFChest.lua
+++ BFChest.lua
@@ -17,7 +17,7 @@ Join Our Discord Server : https://discord.gg/qyDJcKb3gW
 Scipt made by ! MIAFA
 Join Our Discord Server : https://discord.gg/qyDJcKb3gW
 --]]
-getgenv().Chest_Farm = true
+getgenv().Chest_Farm = getgenv().Start_Farm
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 repeat wait() until game:IsLoaded() ~= false
